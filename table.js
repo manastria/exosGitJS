7ac7815f24da2384d027ee015c85f82c9afb7463
Jessica <jessica@manastria.fr>
2014-09-24 11:32:41 +0200

    Prise en compte de table quelconque

--- table.js
+++ table.js
@@ -1,10 +1,10 @@
 // Afficher une table des multiplications
-function display_table()
+function display_table(table)
 {
 	var table_str = "";
 	for(i=1;i<=10;i++) {
-		resultat = 2 * i;
-		table_str += "2 * " + i + " = " + resultat + "<br/>";
+		resultat = table * i;
+		table_str += table + " * " + i + " = " + resultat + "<br/>";
 	}
 	document.getElementById('message').innerHTML = table_str;
 }
